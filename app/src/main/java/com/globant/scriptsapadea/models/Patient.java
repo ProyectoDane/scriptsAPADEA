0f458a4af889e94f8d0b9fdd15d3c016c74edb49
Guillermo Rosales <guillermo.rosales@globant.com>
2015-06-01 17:43:50 -0700

    fetched conflict solved, there is an error in ScriptsSelectorGridAdapter

--- app/src/main/java/com/globant/scriptsapadea/models/Patient.java
+++ app/src/main/java/com/globant/scriptsapadea/models/Patient.java
@@ -1,5 +1,8 @@
 package com.globant.scriptsapadea.models;
 
+import com.globant.scriptsapadea.R;
+
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -66,4 +69,18 @@ public class Patient {
                 ", scriptList=" + scriptList +
                 '}';
     }
+
+
+    public static List<Patient> fetchAllPatients() {
+
+        LinkedList listScript = new LinkedList<Patient>();
+        listScript.add(new Patient("0", "Pepe", R.drawable.avatar_placeholder));
+        listScript.add(new Patient("1", "Juan", R.drawable.avatar_placeholder));
+        listScript.add(new Patient("2", "Marcelo", R.drawable.avatar_placeholder));
+        listScript.add(new Patient("3", "Roberto", R.drawable.avatar_placeholder));
+        listScript.add(new Patient("4", "Juan", R.drawable.avatar_placeholder));
+
+        return listScript;
+    }
+
 }
