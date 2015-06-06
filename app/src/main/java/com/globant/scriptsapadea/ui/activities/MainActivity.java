98a96e21c881b1085737201b06a1d625c1fc745e
Nicolás Quartieri <nicolas.quartieri@gmail.com>
2015-06-06 16:54:51 -0300

    NEW: toolbar added

--- app/src/main/java/com/globant/scriptsapadea/ui/activities/MainActivity.java
+++ app/src/main/java/com/globant/scriptsapadea/ui/activities/MainActivity.java
@@ -1,14 +1,15 @@
 package com.globant.scriptsapadea.ui.activities;
 
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.globant.scriptsapadea.R;
 import com.globant.scriptsapadea.navigator.anim.SlidingUpAnimation;
 import com.globant.scriptsapadea.ui.fragments.PatientListFragment;
 
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
 /**
  * @author nicolas.quartieri.
@@ -16,10 +17,16 @@ import roboguice.inject.ContentView;
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity implements PatientListFragment.PatientListFragmentListener {
 
+    @InjectView(R.id.toolbar_actionbar)
+    private Toolbar toolbar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // TODO Place in BaseActivity
+        setSupportActionBar(toolbar);
+        
         if (savedInstanceState == null) {
             navigator.to(new PatientListFragment()).noPush().navigate();
         }
--- app/src/main/java/com/globant/scriptsapadea/ui/activities/ScreenPlayActivity.java
+++ app/src/main/java/com/globant/scriptsapadea/ui/activities/ScreenPlayActivity.java
@@ -2,19 +2,27 @@ package com.globant.scriptsapadea.ui.activities;
 
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.globant.scriptsapadea.R;
 import com.globant.scriptsapadea.ui.fragments.PictureFragment;
 import com.globant.scriptsapadea.ui.fragments.ScreenPlayFragment;
 
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
 @ContentView(R.layout.activity_screenplay)
 public class ScreenPlayActivity extends BaseActivity implements ScreenPlayFragment.OnScreenplayChangeFragmentListener {
 
+    @InjectView(R.id.toolbar_actionbar)
+    private Toolbar toolbar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // TODO Place in BaseActivity
+        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null)
             navigator.to(new ScreenPlayFragment()).noPush().navigate();
